70627666dedeb450bcc96e6a1d5a8dc8c2c9165c
Matt S Trout <mst@shadowcat.co.uk>
2007-07-28 10:33:01 +0000

    Jesse's turn to forget the svk add, I do believe

---
+++ lib/DBIx/Class/JDBICompat/Handle/SQLite.pm
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+package Jifty::DBI::Handle::SQLite;
+
+use base qw/Jifty::DBI::Handle/;
+
+1;
---
+++ lib/DBIx/Class/JDBICompat/Handle/mysql.pm
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+package Jifty::DBI::Handle::mysql;
+
+use base qw/Jifty::DBI::Handle/;
+
+1;
